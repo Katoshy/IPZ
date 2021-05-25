52ddfc22e458ba11d0dafaeeb3d7f7528e2897db
Nickolas <37816963+NickolasMol@users.noreply.github.com>
2021-05-25 08:50:02 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+
+namespace IPZ
+{
+    class Image { }
+    class AI_Algorithm
+    {
+
+    }
+    class RealTimeGenerator
+    {
+
+    }
+    class QuerryExecutor
+    {
+
+    }
+    class Account
+    {
+        String Name;
+        int ID;
+    }
+    class Post
+    {
+        Account PostAuthor;
+        Image[] Images;
+        Comment [] Comments;
+        String Description;
+    }
+    class Comment
+    {
+        Account CommentAuthor;
+        String message;
+    }
+    class User
+    {
+        Account account;
+        void Regestration() { }
+        void Scanning() { }
+        void ARView() { }
+        void PostView() { }
+        void CreatePost() { }
+        void CreateComment() { }
+    }
+    class CameraView
+    {
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
